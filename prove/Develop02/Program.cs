48936442415046c9048d9b0cf1237c93fdc65e8d
brycewoodland <bwoodlan@byui.edu>
2023-10-03 10:47:58 -0600

    journal

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        int choice = 0;
+
+        while (choice != 5)
+            {
+                Console.WriteLine("1. Write a new entry");
+                Console.WriteLine("2. Display the journal");
+                Console.WriteLine("3. Save the journal to a file");
+                Console.WriteLine("4. Load the journal from a file");
+                Console.WriteLine("5. Exit");
+                Console.WriteLine("Select an option: ");
+
+                string action = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case 1:
+                        string prompt = journal.GetPrompt();
+                        Console.WriteLine("Enter your journal entry:");
+                        string entryText = Console.ReadLine();
+                        journal.AddEntry(prompt, entryText);
+                        break;
+                    case 2:
+                        journal.Display();
+                        break;
+                    case 3:
+                        Console.Write("Enter a filename to save the journal: ");
+                        string FileName = Console.ReadLine();
+                        journal.SaveToFile(fileName);
+                        break;
+                    case 4:
+                        Console.Write("Enter a filename to load the journal: ");
+                        FileName = Console.ReadLine();
+                        journal.LoadFromFile(fileName);
+                    case 5:
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid option. Please choose a valid option.");
+                        break;                
+                }
+            }
+        }
     }
-}
