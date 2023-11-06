978826c6633b09f012c01a7e5956c56536408ea1
brycewoodland <bwoodlan@byui.edu>
2023-11-06 10:09:43 -0700

    Learning05 complete

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square sq = new Square("red", 5);
+        shapes.Add(sq);
+
+        Rectangle r = new Rectangle("blue", 4, 3);
+        shapes.Add(r);
+
+        Circle c = new Circle("orange", 2);
+        shapes.Add(c);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
